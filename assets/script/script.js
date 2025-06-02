58a027ef1308b7a8e7b7fba1a4f6483783325e69
01Chloe <allier.chloe@gmaim.com>
2025-06-02 10:22:27 +0200

    add gender

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,10 +2,12 @@ const form = document.querySelector(".form");
 const lname = document.querySelector("#lname");
 const fname = document.querySelector("#fname");
 const age = document.querySelector("#age");
+const gender = document.getElementsByName("gender");
 const formValueContainer = document.querySelector(".form-value-container");
 const displayLName = document.querySelector(".display-lname");
 const displayFName = document.querySelector(".display-fname");
 const displayAge = document.querySelector(".display-age");
+const displayGender = document.querySelector(".display-gender");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -20,4 +22,7 @@ function displayFormValue() {
   displayLName.innerHTML = lname.value;
   displayFName.innerHTML = fname.value;
   displayAge.innerHTML = age.value;
+  for (i = 0; i < gender.length; i++) {
+    if (gender[i].checked) displayGender.innerHTML = gender[i].value;
+  }
 }

0447b5e54837fcf696d4f8128d35add3f010b884
01Chloe <allier.chloe@gmaim.com>
2025-06-02 10:31:58 +0200

    add region

--- assets/script/script.js
+++ assets/script/script.js
@@ -4,12 +4,14 @@ const fname = document.querySelector("#fname");
 const age = document.querySelector("#age");
 const gender = document.getElementsByName("gender");
 const job = document.querySelector("#job");
+const region = document.querySelector("#region");
 const formValueContainer = document.querySelector(".form-value-container");
 const displayLName = document.querySelector(".display-lname");
 const displayFName = document.querySelector(".display-fname");
 const displayAge = document.querySelector(".display-age");
 const displayGender = document.querySelector(".display-gender");
 const displayJob = document.querySelector(".display-job");
+const displayRegion = document.querySelector(".display-region");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -21,6 +23,7 @@ form.addEventListener("submit", (e) => {
     if (gender[i].checked) gender[i].checked = false;
   }
   job.value = "";
+  region.value = "";
 });
 
 function displayFormValue() {
@@ -32,4 +35,5 @@ function displayFormValue() {
     if (gender[i].checked) displayGender.innerHTML = gender[i].value;
   }
   displayJob.innerHTML = job.value;
+  displayRegion.innerHTML = region.value;
 }
